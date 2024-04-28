1f2ee8c4cf590b5479854fe3d81311c38d66ed54
chimdike21 <138835446+chimdike21@users.noreply.github.com>
2024-04-28 20:40:39 +0100

    added file

--- script.js
+++ script.js
@@ -5,3 +5,20 @@ price:$50},
     {id:2, brand:'Honda', model:'Civic', year:2021, price:$60},
 
 ]
+
+function displayCars(){
+    const carList= document.getElementById('carList')
+    carList.innerHTML=""
+    cars.forEach(car=>{
+        const carItem=document.createElement('div')
+        carItem.classList.add('car')
+        carItem.innerHTML=`
+        <h2>${car.brand}${car.model}</h2>
+        <p>Year:${car.year}</p>
+        <p>Price per day:$${car.price}</p>`
+
+        carList.appendChild(carItem)
+    })
+}
+
+displayCars()
